dc35fd21f4c478ad377c695700190e5bec7a9485
Carlosedukj <c.eduardoss@outlook.com>
2026-04-18 18:03:18 -0300

    Iniciando o projeto

---
+++ script.js
@@ -0,0 +1,9 @@
+// Capturando os elementos.
+const inputContent = document.getElementById("input-content")
+const errorMessage = document.getElementById("error-message")
+const buttonAdd = document.getElementById("button-add")
+const buttonCompleted = document.getElementById("completed-task")
+const buttonPending = document.getElementById("pending-task")
+const buttonAllTask = document.getElementById("all-task")
+const listContent = document.getElementById("list-content")
+
